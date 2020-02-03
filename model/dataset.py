9ed96d94bb749b4e1ee23e8423b5220fa04f0c75
Gundu <806707@cognizant.com>
2020-02-03 12:45:02 +0530

    Dask Integrated with dataset class for datasets larger than 3 MB

--- model/dataset.py
+++ model/dataset.py
@@ -2,9 +2,12 @@ import numpy as np
 from pandas import DataFrame, Series
 import pandas as pd
 import pprint
-
+import os
+import dask.dataframe as dd
 
 class Dataset(DataFrame):
+    possible_label_headers = ['classes', 'class',
+                       'labels', 'label', 'output', 'problems']
     def __init__(self, task='Supervised_Classification', data=None, labels=None, *args, **kwargs):
         if(task == 'Supervised_Classification'):
             self._supervised_classification_init(data, labels,*args, **kwargs)
@@ -18,23 +21,44 @@ class Dataset(DataFrame):
 
     def __call__(self):
         about_dict = {}
-        about_dict['rows'] = self.data.shape[0]
-        about_dict['columns'] = self.data.shape[1]
-        about_dict['Unique classes'] = len(self.labels.unique())
-        about_dict['elements'] = self.data.shape[0]*self.data.shape[1]
+        about_dict['Using Dask'] = 'Yes' if self.use_dask else 'No'
+        if(self.use_dask):
+            shape = self.data.shape.compute()
+            about_dict['rows'] = shape[0]
+            about_dict['columns'] = shape[1]
+            about_dict['Unique classes'] = len(self.labels.compute().unique())             
+        else:
+            about_dict['rows'] = self.data.shape[0]
+            about_dict['columns'] = self.data.shape[1]
+            about_dict['Unique classes'] = len(self.labels.unique())
+            about_dict['elements'] = self.data.shape[0]*self.data.shape[1]            
         return about_dict
 
     def _check_for_classification(self):
         self.preprocesses = []
-        if(Dataset.no_of_nans(self.data) != 0):
-            self.preprocesses.append('NANs')
-        try:
-            int(self.labels.iloc[0])
-        except:
-            self.preprocesses.append('Non-numeric labels')
-        
+        if(self.use_dask):
+            if(Dataset.no_of_nans(self.data.compute()) != 0):
+                self.preprocesses.append('NANs')
+        else:
+            if(Dataset.no_of_nans(self.data) != 0):
+                self.preprocesses.append('NANs')
+            try:
+                int(self.labels.iloc[0])
+            except:
+                self.preprocesses.append('Non-numeric labels')
+            
+            if(self.preprocesses == []):
+                self.preprocesses.append('All clear')
         return self.preprocesses
     
+    def _supervised_dask_init(self):
+        dataset = dd.read_csv(self.path)
+        names = [i.lower() for i in dataset.columns]
+        #global possible_label_headers
+        if(any(i in names for i in self.possible_label_headers)):
+            self.data = dataset.iloc[:, :-1]
+            self.labels = dataset.iloc[:, -1]
+
     def _supervised_classification_init(self, data, labels,*args,**kwargs):
         path = kwargs.pop('path', None)
         data_path = kwargs.pop('data_path', None)
@@ -45,21 +69,26 @@ class Dataset(DataFrame):
         self.path = path
         self.data_path = data_path
         self.labels_path = labels_path
+        self.use_dask = False
 
-        label_names = ['classes', 'class',
-                       'labels', 'label', 'output', 'problems']
+        #global possible_label_headers
 
         if (not (path == None)):
             if ('.csv' not in path):
                 print('Only CSV files are supported')
             else:
-                dataset = pd.read_csv(path)
-                names = dataset.columns
-                if(any(i in names for i in label_names)):
-                    self.data = dataset.iloc[:, :-1]
-                    self.labels = dataset.iloc[:, -1]
+                file_size = os.stat(path).st_size/(1024*1024)
+                if(file_size < 3):
+                    dataset = pd.read_csv(path)
+                    names = dataset.columns
+                    if(any(i in names for i in self.possible_label_headers)):
+                        self.data = dataset.iloc[:, :-1]
+                        self.labels = dataset.iloc[:, -1]
+                    else:
+                        print('Labels are not found in the path')
                 else:
-                    print('Labels are not found in the path')
+                    self.use_dask = True
+                    self._supervised_dask_init()
 
         elif(not(data_path == None)):
             try:
@@ -91,7 +120,23 @@ class Dataset(DataFrame):
     def no_of_nans(set_to_be_checked):
         return set_to_be_checked.isnull().sum().sum()
 
+    def train_test_split(self):
+        if(self.use_dask):
+            from dask_ml.model_selection import train_test_split            
+        else:
+            from sklearn.model_selection import train_test_split
+        
+        self.X_train,self.X_test,self.y_train,self.y_test = train_test_split(self.data, self.labels)
+        return (self.X_train,self.X_test,self.y_train,self.y_test)
+
 
 if (__name__ == '__main__'):
-    dataset = Dataset(path=r'C:\Users\806707\Downloads\kc2 (1).csv')    
+    #dataset = Dataset(path=r'C:\Users\806707\Downloads\kc2 (1).csv')  
+    dataset = Dataset(path=r'C:\Users\806707\Downloads\hill.csv')
+    print(dataset())  
     print(dataset._check_for_classification())
+    (a,b,c,d) = dataset.train_test_split()
+    print(a.shape)
+    print(b.shape)
+    print(c.shape)
+    print(d.shape)

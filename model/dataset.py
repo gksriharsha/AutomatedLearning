85b6aa2851e0a9741a733b2e97589be9216f1d25
Gundu <806707@cognizant.com>
2020-02-26 14:56:29 +0530

    Added fetching from URL link to csv

--- model/dataset.py
+++ model/dataset.py
@@ -62,8 +62,6 @@ class Dataset(DataFrame):
             except AttributeError:
                 if(Dataset.no_of_nans(self.X_train.compute() != 0)):
                     self.preprocesses.append('NANs')
-
-
         else:
             try:
                 if(Dataset.no_of_nans(self.data) != 0):   
@@ -84,7 +82,10 @@ class Dataset(DataFrame):
         return self.preprocesses
 
     def _supervised_dask_init(self):
-        dataset = dd.read_csv(self.path,na_values=['?',"?",'-',"-",'_',"_",'\'?\'','\"?\"'])
+        if(self.path):
+            dataset = dd.read_csv(self.path,na_values=['?',"?",'-',"-",'_',"_",'\'?\'','\"?\"'])
+        if(self.url_path):
+            dataset = dd.read_csv(self.url_path,na_values=['?',"?",'-',"-",'_',"_",'\'?\'','\"?\"'])
         names = [i.lower() for i in dataset.columns]
         #global possible_label_headers
         if(any(i in names for i in self.possible_label_headers)):
@@ -94,16 +95,17 @@ class Dataset(DataFrame):
                 dataset.drop(label_header[0],axis=1)
                 self.data = dataset
                 self.data = self.data.apply(pd.to_numeric,axis=1, args=('coerce',),meta = self.data.dtypes)
-                        
 
     def _supervised_classification_init(self, data, labels, *args, **kwargs):
         path = kwargs.pop('path', None)
+        url_path = kwargs.pop('url_path',None)
         data_path = kwargs.pop('data_path', None)
         labels_path = kwargs.pop('labels_path', None)
 
         super(Dataset, self).__init__(*args, **kwargs)
 
         self.path = path
+        self.url_path = url_path
         self.data_path = data_path
         self.labels_path = labels_path
         self.use_dask = False
@@ -123,8 +125,12 @@ class Dataset(DataFrame):
                     dataset = pd.read_csv(path,na_values=['?',"?",'-',"-",'_',"_",'\'?\'','\"?\"'])
                     names = [i.lower() for i in dataset.columns]
                     if(any(i in names for i in self.possible_label_headers)):
-                        self.data = dataset.iloc[:, :-1]
-                        self.labels = dataset.iloc[:, -1]                        
+                        label_header = list(set(names).intersection(set(self.possible_label_headers)))
+                        if(len(label_header) == 1):                 
+                            self.labels = dataset.loc[:,list(map(lambda x:True if x.lower() == label_header[0] else False,dataset.columns))]
+                            dataset.drop(label_header[0],axis=1)
+                            self.data = dataset
+                            self.data = self.data.apply(pd.to_numeric,axis=1, args=('coerce',),meta = self.data.dtypes)
                     else:
                         print('Labels are not found in the path')
                 else:
@@ -133,7 +139,7 @@ class Dataset(DataFrame):
 
         elif(not(data_path == None)):
             try:
-                if('.csv' not in kwargs['data_path']):
+                if('.csv' not in data_path):
                     print('Only CSV files are supported')
                 else:
                     try:
@@ -145,6 +151,27 @@ class Dataset(DataFrame):
             except Exception as e:
                 print(e)
 
+        elif(not(url_path == None)):
+            if ('http' not in url_path):
+                print('Only URL files are supported')
+            else:
+                downloaded_csv = dd.read_csv(url_path,na_values=['?',"?",'-',"-",'_',"_",'\'?\'','\"?\"'])
+                file_size = downloaded_csv.memory_usage().sum().compute()/(1024*1024)
+                if(file_size < 3):
+                    dataset = downloaded_csv.compute()                    
+                    names = [i.lower() for i in dataset.columns]
+                    if(any(i in names for i in self.possible_label_headers)):
+                        label_header = list(set(names).intersection(set(self.possible_label_headers)))
+                        if(len(label_header) == 1):                 
+                            self.labels = dataset.loc[:,list(map(lambda x:True if x.lower() == label_header[0] else False,dataset.columns))]
+                            dataset.drop(label_header[0],axis=1)
+                            self.data = dataset
+                            self.data = self.data.apply(pd.to_numeric,axis=1, args=('coerce',),meta = self.data.dtypes)
+                    else:
+                        print('Labels are not found in the path')
+                else:
+                    self.use_dask = True
+                    self._supervised_dask_init()
         else:
             if (isinstance(data, DataFrame)):
                 self.data = data
@@ -239,8 +266,8 @@ class Dataset(DataFrame):
 if(__name__ == '__main__'):
     #dataset = Dataset(path=r'C:\Users\806707\Downloads\kc2 (1).csv')
     #dataset = Dataset(path=r'C:\Users\806707\Downloads\hill.csv')
-    #dataset = Dataset(path=r'C:\Users\806707\Downloads\higgs.csv')
-    dataset = Dataset(path=r'C:\Users\806707\Downloads\arrhythmia.csv')
+    dataset = Dataset(path=r'C:\Users\806707\Downloads\higgs.csv')
+    #dataset = Dataset(path=r'C:\Users\806707\Downloads\arrhythmia.csv')
     #print(dataset())
     print(dataset._impute_data(imputation_method='KNN'))
     print(dataset())
